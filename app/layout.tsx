2e7c1a7c13dfcfb06d48936af032c032ba241bec
Zakariya Baig <85746585+ZakariyaBaig@users.noreply.github.com>
2025-06-09 11:53:07 +0100

    Add initial gatorade styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +12,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const proximanova = localFont({
+  variable: "--font-proximanova",
+  src: "../public/proximanova-extrabldit - Copy.otf",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${proximanova.variable} antialiased`}
       >
         {children}
       </body>
